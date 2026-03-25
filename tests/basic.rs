f1da61eb2ca45551a88cdee1a5cf500f2afa582c
l1h3r <5410284+l1h3r@users.noreply.github.com>
2026-03-25 13:23:46 -0700

    Fix clippy errors

--- tests/basic.rs
+++ tests/basic.rs
@@ -29,34 +29,34 @@ fn assert_json_number_err(input: u64) {
 
 #[test]
 fn test_rfc_appendix_b() {
-  assert_json_number(0x0000000000000000, "0"); // Zero
-  assert_json_number(0x8000000000000000, "0"); // Minus zero
-  assert_json_number(0x0000000000000001, "5e-324"); // Min pos number
-  assert_json_number(0x8000000000000001, "-5e-324"); // Min neg number
-  assert_json_number(0x7fefffffffffffff, "1.7976931348623157e+308"); // Max pos number
-  assert_json_number(0xffefffffffffffff, "-1.7976931348623157e+308"); // Max neg number
-  assert_json_number(0x4340000000000000, "9007199254740992"); // Max pos int
-  assert_json_number(0xc340000000000000, "-9007199254740992"); // Max neg int
-  assert_json_number(0x4430000000000000, "295147905179352830000"); // ~2**68
+  assert_json_number(0x0000_0000_0000_0000, "0"); // Zero
+  assert_json_number(0x8000_0000_0000_0000, "0"); // Minus zero
+  assert_json_number(0x0000_0000_0000_0001, "5e-324"); // Min pos number
+  assert_json_number(0x8000_0000_0000_0001, "-5e-324"); // Min neg number
+  assert_json_number(0x7fef_ffff_ffff_ffff, "1.7976931348623157e+308"); // Max pos number
+  assert_json_number(0xffef_ffff_ffff_ffff, "-1.7976931348623157e+308"); // Max neg number
+  assert_json_number(0x4340_0000_0000_0000, "9007199254740992"); // Max pos int
+  assert_json_number(0xc340_0000_0000_0000, "-9007199254740992"); // Max neg int
+  assert_json_number(0x4430_0000_0000_0000, "295147905179352830000"); // ~2**68
 
-  assert_json_number_err(0x7fffffffffffffff); // NaN
-  assert_json_number_err(0x7ff0000000000000); // Infinity
+  assert_json_number_err(0x7fff_ffff_ffff_ffff); // NaN
+  assert_json_number_err(0x7ff0_0000_0000_0000); // Infinity
 
-  assert_json_number(0x44b52d02c7e14af5, "9.999999999999997e+22");
-  assert_json_number(0x44b52d02c7e14af6, "1e+23");
-  assert_json_number(0x44b52d02c7e14af7, "1.0000000000000001e+23");
-  assert_json_number(0x444b1ae4d6e2ef4e, "999999999999999700000");
-  assert_json_number(0x444b1ae4d6e2ef4f, "999999999999999900000");
-  assert_json_number(0x444b1ae4d6e2ef50, "1e+21");
-  assert_json_number(0x3eb0c6f7a0b5ed8c, "9.999999999999997e-7");
-  assert_json_number(0x3eb0c6f7a0b5ed8d, "0.000001");
-  assert_json_number(0x41b3de4355555553, "333333333.3333332");
-  assert_json_number(0x41b3de4355555554, "333333333.33333325");
-  assert_json_number(0x41b3de4355555555, "333333333.3333333");
-  assert_json_number(0x41b3de4355555556, "333333333.3333334");
-  assert_json_number(0x41b3de4355555557, "333333333.33333343");
-  assert_json_number(0xbecbf647612f3696, "-0.0000033333333333333333");
-  assert_json_number(0x43143ff3c1cb0959, "1424953923781206.2"); // Round to even
+  assert_json_number(0x44b5_2d02_c7e1_4af5, "9.999999999999997e+22");
+  assert_json_number(0x44b5_2d02_c7e1_4af6, "1e+23");
+  assert_json_number(0x44b5_2d02_c7e1_4af7, "1.0000000000000001e+23");
+  assert_json_number(0x444b_1ae4_d6e2_ef4e, "999999999999999700000");
+  assert_json_number(0x444b_1ae4_d6e2_ef4f, "999999999999999900000");
+  assert_json_number(0x444b_1ae4_d6e2_ef50, "1e+21");
+  assert_json_number(0x3eb0_c6f7_a0b5_ed8c, "9.999999999999997e-7");
+  assert_json_number(0x3eb0_c6f7_a0b5_ed8d, "0.000001");
+  assert_json_number(0x41b3_de43_5555_5553, "333333333.3333332");
+  assert_json_number(0x41b3_de43_5555_5554, "333333333.33333325");
+  assert_json_number(0x41b3_de43_5555_5555, "333333333.3333333");
+  assert_json_number(0x41b3_de43_5555_5556, "333333333.3333334");
+  assert_json_number(0x41b3_de43_5555_5557, "333333333.33333343");
+  assert_json_number(0xbecb_f647_612f_3696, "-0.0000033333333333333333");
+  assert_json_number(0x4314_3ff3_c1cb_0959, "1424953923781206.2"); // Round to even
 }
 
 #[test]
